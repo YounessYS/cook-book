e72b62b35f27472f9d8900d1b30de25e818abc38
Youness <107858907+YounessYS@users.noreply.github.com>
2024-10-10 20:04:43 +0100

    create recipe card component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,37 @@
+import { Heart, HeartPulse, Search, Soup } from "lucide-react";
+import RecipeCard from "../components/RecipeCard";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="p-10 flex-1">
+      <div className="max-w-screen-lg mx-auto">
+        <form>
+          <label className="input shadow-md flex items-center gap-2">
+            <Search size={"24"} />
+            <input
+              type="text"
+              className="text-sm md:text-md grow text-white placeholder-white"
+              placeholder="What would you like to cook today?"
+            />
+          </label>
+        </form>
+        <h1 className="font-bold text-3xl md:text-5xl mt-4">
+          Recommended Recipes
+        </h1>
+        <p className="font-semibold ml-1 my-2 text-sm tracking-tight">
+          Popular choices
+        </p>
+
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+        </div>
+      </div>
+    </div>
+  );
 }
