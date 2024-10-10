fc1ee87a670428baa48ff93aa8987cc1414aba4b
Youness <107858907+YounessYS@users.noreply.github.com>
2024-10-10 19:01:52 +0100

    add sidebar including mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import HomePage from "./pages/HomePage";
+import FavouritesPage from "./pages/FavouritesPage";
+
 function App() {
   return (
-    <>
-      <h1 className="text-blue-800">Hello world</h1>
-    </>
+    <div className="flex">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
+    </div>
   );
 }
 
